8f22ff900f27de75b66dfcc8945ba868c35ea123
AndreyK <ponachen@mail.ru>
2015-09-19 21:36:53 +0600

    next commit

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var http = require('http');
+var express = require('express')
+var app = express()
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/', function (req, res) {
+    res.send('Hello World3')
+})
 
-    res.end('Hello World1\n');
 
-}).listen(8080);
-console.log('listening 8080');
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:7654321@localhost:5432/AndeyBD';
+
+var client = new pg.Client(connectionString);
+client.connect();
+var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', function() { client.end(); });
+
+
+app.listen(3000)
