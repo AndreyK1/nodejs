7266881b96307ef8ba9e910fea50b9ee71afbc34
unknown <Kirilyuk@phantom.office.trisoftrus.com>
2015-09-23 15:59:22 +0300

    w Token commit

--- models/User.js
+++ models/User.js
@@ -9,13 +9,25 @@ var UserSchema   = new Schema({
 });
 
 module.exports = mongoose.model('User', UserSchema);
-    */
+   */
 exports.findOne = function(user, func1) {
     console.log('findOne user-'+user.email + ' password' +user.password)
     user1 = {email: user.email,
-            password: user.password//,
-            //token : 'token'
+            password: user.password,
+            token : 'authenticate token'
     }
-
-    func1(null,user1);
+	if(!user.save){//если мы не сохраняем
+		func1(null,user1);
+	}else{
+		func1(null,null);
+	}
 };
+exports.newUser ={
+    email: '',
+    password: '',
+    token: '',
+	secret: '5553rwewerwer',
+	save:function(par){
+		par(null,this);
+	}
+}
--- server_token.js
+++ server_token.js
@@ -1,8 +1,9 @@
+//CRUD
 // Required Modules
 var express    = require("express");
 //var morgan     = require("morgan");
 var bodyParser = require("body-parser");
-//var jwt        = require("jsonwebtoken");
+var jwt        = require("jsonwebtoken");
 //var mongoose   = require("mongoose");
 var app        = express();
 
@@ -15,7 +16,7 @@ var User     = require('./models/User');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-  /*
+ 
 //app.use(morgan("dev"));
 
 app.use(function(req, res, next) {
@@ -24,14 +25,14 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
-*/
+
 
 
 app.post('/authenticate', function(req, res) {
     //console.log(req.body.email);
     //User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
 
-    User.findOne({email:req.body.email, password: req.body.pass}, function(err, user) {
+    User.findOne({email:req.body.email, password: req.body.password}, function(err, user) {
         if (err) {
             res.json({
                 type: false,
@@ -41,8 +42,8 @@ app.post('/authenticate', function(req, res) {
             if (user) {
                 res.json({
                     type: true,
-                    data: user//,
-                    //token: user.token
+                    data: user,
+                    token: user.token
                 });
             } else {
                 res.json({
@@ -54,9 +55,9 @@ app.post('/authenticate', function(req, res) {
     });
 });
 
-/*
+
 app.post('/signin', function(req, res) {
-    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+    User.findOne({email: req.body.email, password: req.body.password, save:'yes'}, function(err, user) {
         if (err) {
             res.json({
                 type: false,
@@ -69,24 +70,34 @@ app.post('/signin', function(req, res) {
                     data: "User already exists!"
                 });
             } else {
-                var userModel = new User();
+                //var userModel = new User();
+				userModel = User.newUser;
                 userModel.email = req.body.email;
                 userModel.password = req.body.password;
+				//userModel.token = 'signin token';
+				/*userModel.token = jwt.sign(userModel, process.env.JWT_SECRET || 'secret key'); //process.env.JWT_SECRET);
+				res.json({
+                    type: true,
+                    data: userModel
+                });*/
                 userModel.save(function(err, user) {
-                    user.token = jwt.sign(user, process.env.JWT_SECRET);
-                    user.save(function(err, user1) {
+                    userModel.token = jwt.sign(userModel, process.env.JWT_SECRET || 'secret key');
+                   // userModel.token = jwt.sign(userModel, process.env.JWT_SECRET );//for exeption
+					userModel.save(function(err, user1) {
                         res.json({
                             type: true,
                             data: user1,
                             token: user1.token
                         });
                     });
+					
                 })
             }
         }
     });
 });
 
+/*
 app.get('/me', ensureAuthorized, function(req, res) {
     User.findOne({token: req.token}, function(err, user) {
         if (err) {
@@ -115,11 +126,11 @@ function ensureAuthorized(req, res, next) {
         res.send(403);
     }
 }
-
+*/
 process.on('uncaughtException', function(err) {
     console.log(err);
 });
-*/
+
 // Start Server
 app.listen(port, function () {
     console.log( "Express server listening on port -- " + port);
