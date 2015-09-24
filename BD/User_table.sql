3a596c70e87a74944159bfc585b55801896b5490
AndreyK <ponachen@mail.ru>
2015-09-24 20:24:47 +0600

    1

---
+++ BD/User_table.sql
@@ -0,0 +1,18 @@
+﻿-- Table: users
+
+-- DROP TABLE users;
+
+CREATE TABLE users
+(
+  id integer NOT NULL DEFAULT nextval('"Users_id_seq"'::regclass),
+  email character varying(255),
+  password character varying(255),
+  "createdAt" timestamp with time zone NOT NULL,
+  "updatedAt" timestamp with time zone NOT NULL,
+  CONSTRAINT "Users_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE users
+  OWNER TO postgreadmin;
